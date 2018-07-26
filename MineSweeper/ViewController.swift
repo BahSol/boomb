6ffa57dce7f5db2273aa62906d561f1a9f6ed66a
Generic <s5071902@g23p220z062003.student.ad.griffith.edu.au>
2018-07-26 17:35:40 +1000

    Initial Commit

---
+++ MineSweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Bahar Soleimani on 26/7/18.
+//  Copyright © 2018 Bahar Soleimani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
