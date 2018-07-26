e7b7decb2fca7b000036c5c5e36393dcf6995cb2
Generic <s5071902@g23p220z062003.student.ad.griffith.edu.au>
2018-07-26 17:57:31 +1000

    initial

--- MineSweeper/ViewController.swift
+++ MineSweeper/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var boardView: UIView!
+    
+    @IBOutlet weak var movesLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
